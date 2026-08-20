12409fe77b27f7ba17cf27500e2a06b52e4b1f09
Zetagames <davidzb16@gmail.com>
2026-08-19 21:20:13 -0600

    fix: disable corsair headset mouse pointer

--- .config/hypr/custom/general.lua
+++ .config/hypr/custom/general.lua
@@ -1,1 +1,3 @@
 
+hl.device({ name = "corsair-corsair-slipstream-multi-device-receiver", enabled = false })
+hl.device({ name = "corsair-corsair-slipstream-multi-device-receiver-consumer-control-1", enabled = false })
