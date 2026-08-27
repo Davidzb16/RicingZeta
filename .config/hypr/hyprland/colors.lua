60e207ddcbd665f971c4ef50121f184dd825061a
Zetagames <davidzb16@gmail.com>
2026-08-26 21:00:48 -0600

    Update hyprland configs

--- .config/hypr/hyprland/colors.lua
+++ .config/hypr/hyprland/colors.lua
@@ -1,16 +1,16 @@
 hl.config({
     general = {
         col = {
-            active_border   = "rgba(53434177)",
-            inactive_border = "rgba(23191833)",
+            active_border   = "rgba(51434677)",
+            inactive_border = "rgba(22191b33)",
         },
     },
     misc = {
-        background_color = "rgba(1a1110FF)",
+        background_color = "rgba(191113FF)",
     },
 })
 
 hl.window_rule({
     match        = { pin = 1 },
-    border_color = "rgba(ffb4a9AA) rgba(ffb4a977)",
+    border_color = "rgba(ffb1c4AA) rgba(ffb1c477)",
 })
